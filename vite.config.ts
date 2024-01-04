0a97d91c42efabcb75abbc882594a31675b76572
Patryk Szram <patryk@szram.co>
2024-01-04 13:22:08 +0100

    Update GH action with predeploy hook

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { outDir: 'dist' },
   plugins: [basicSsl(), vue(), progress()],
   resolve: {
     alias: {
