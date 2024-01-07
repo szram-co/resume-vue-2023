8759e8c1c870c1bb962272d50998ae8c08be4874
Patryk Szram <patryk@szram.co>
2024-01-07 19:31:12 +0100

    Update resume

--- src/App.vue
+++ src/App.vue
@@ -329,7 +329,7 @@ const { execute } = useChallengeV3('submit')
 const positionTitle = 'Fullstack Web Developer'
 const contactPhone = ref('+48795884999')
 const contactEmail = ref('patryk@szram.co')
-const resumePdf = computed(() => `/patryk-szram-resume-${locale.value}.pdf`)
+const resumePdf = computed(() => `/patryk-szram-resume-en.pdf`)
 
 const { t, locale } = useI18n({ useScope: 'global' })
 const currentLocation = `${t('city.gdz')}, ${t('country.pl')}`
@@ -421,7 +421,7 @@ const data = reactive({
           positionDesc: computed(() => t('jobs.raiffeisen.senior')),
           positionDate: {
             current: true, // TODO: pls change that
-            end: computed(() => `${t('dates.present')}`),
+            end: computed(() => `${t('months.dec').slice(0, 3)} 2023`),
             start: computed(() => `${t('months.jun').slice(0, 3)} 2021`)
           }
         },
@@ -507,8 +507,8 @@ const data = reactive({
           positionDesc: computed(() => t('jobs.flash-media.developer')),
           positionDate: {
             current: false,
-            end: computed(() => `${t('months.nov').slice(0, 3)} 2017`),
-            start: computed(() => `${t('months.mar').slice(0, 3)} 2015`)
+            end: computed(() => `${t('months.mar').slice(0, 3)} 2015`),
+            start: computed(() => `${t('months.nov').slice(0, 3)} 2011`)
           }
         }
       ]
